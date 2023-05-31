1458a59f6bb4181822324cc34efa19e69c6a9c69
Karol Wiśniewski <karol.wisniewski2901@gmail.com>
2023-05-31 18:54:29 +0200

    add show location

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,53 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:app/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String locationMessage = 'Current location of the User';
+  late String lat;
+  late String long;
+
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied.');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request');
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+
+  void _liveLocation() {
+    LocationSettings locationSettings = const LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100,
+    );
+    Geolocator.getPositionStream(locationSettings: locationSettings)
+        .listen((Position position) {
+      lat = position.latitude.toString();
+      long = position.longitude.toString();
+      setState(() {
+        locationMessage = 'Latitude $lat, Longitude: $long';
+      });
+    });
+  }
 
   final User? user = Auth().currentUser;
 
@@ -15,8 +59,16 @@ class HomePage extends StatelessWidget {
     return const Text('Firebase Auth');
   }
 
-  Widget _userUid() {
-    return Text(user?.email ?? 'User email');
+  Widget _userInfo() {
+    return Column(
+      children: [
+        Text('Email: ${user?.email ?? 'Unknown'}'),
+        Text('User ID: ${user?.uid ?? 'Unknown'}'),
+        Text('Email Verified: ${user?.emailVerified ?? false}'),
+        Text(
+            'Last time sign in: ${user?.metadata.lastSignInTime ?? 'Unknown'}'),
+      ],
+    );
   }
 
   Widget _signOutButton() {
@@ -37,8 +89,31 @@ class HomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            _userUid(),
+            Text(
+              'INFO',
+              textAlign: TextAlign.center,
+            ),
+            _userInfo(),
             _signOutButton(),
+            Text(
+              locationMessage,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  _getCurrentLocation().then((value) {
+                    lat = '${value.latitude}';
+                    long = '${value.longitude}';
+                    setState(() {
+                      locationMessage = 'Latitude: $lat, Longitude: $long';
+                    });
+                    _liveLocation();
+                  });
+                },
+                child: const Text('Get Current Location'))
           ],
         ),
       ),
